75be02efb47a1c31e8c84f09dfdefb533915af8d
code2Vici <naborp@cpp.edu>
2017-04-13 17:40:51 -0700

    Updated Home Screen Design

--- CPPTourApp/app/src/main/java/siliconsolutions/cpptourapp/HomeScreen.java
+++ CPPTourApp/app/src/main/java/siliconsolutions/cpptourapp/HomeScreen.java
@@ -1,13 +1,27 @@
 package siliconsolutions.cpptourapp;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
+
+    private Typeface typeF;
+    private TextView homeTextview;
+    //private SearchView searchHint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        typeF = Typeface.createFromAsset(getAssets(), "fonts/Championship.ttf");
+        homeTextview = (TextView) findViewById(R.id.broncoTxtView);
+        homeTextview.setTypeface(typeF, typeF.ITALIC);
+
     }
+
 }
