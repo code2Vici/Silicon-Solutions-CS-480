633439ea4b49a6974099b27806df3743f27d55cf
Erick Rivera <erickrivera2012@gmail.com>
2017-04-13 19:38:28 -0700

    added game

---
+++ tictactoe/tictactoe/TicTacToe/app/src/main/java/com/myapp/project/tictactoe/LoadScreen.java
@@ -0,0 +1,67 @@
+package com.myapp.project.tictactoe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LoadScreen extends Activity {
+    private ArrayList<File> fileArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_load_screen);
+        final ListView lv = (ListView) findViewById(R.id.saveList);
+        File[] files = getFilesDir().listFiles();
+        fileArrayList = new ArrayList<>(Arrays.asList(files));
+        saveArrayAdapter adapter = new saveArrayAdapter(this,0,fileArrayList);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String ret = readFile(fileArrayList.get(i));
+                Intent intent = new Intent(getApplicationContext(),GameActivity.class);
+                intent.putExtra("data",ret);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private String readFile(File f){
+        String content = null;
+        try {
+            FileInputStream inputStream = openFileInput(f.getName());
+
+            byte[] readByte = new byte[inputStream.available()];
+
+            while(inputStream.read(readByte) != -1){
+                content = new String(readByte);
+            }
+            inputStream.close();
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+        return content;
+    }
+
+}
---
+++ tictactoe/tictactoe/TicTacToe/app/src/main/java/com/myapp/project/tictactoe/saveArrayAdapter.java
@@ -0,0 +1,53 @@
+package com.myapp.project.tictactoe;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class saveArrayAdapter extends ArrayAdapter<File>{
+    public ArrayList<File> files;
+    private Context context;
+
+    public saveArrayAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull ArrayList<File> objects) {
+        super(context, resource, objects);
+        files = objects;
+        this.context = context;
+    }
+
+
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        File file = files.get(position);
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).
+                    inflate(R.layout.save_file_item, parent, false);
+        }
+
+        TextView fileName = (TextView) convertView.findViewById(R.id.saveTitle);
+        ImageButton delete = (ImageButton) convertView.findViewById(R.id.deleteSave);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File f = files.remove(position);
+                f.delete();
+                notifyDataSetChanged();
+            }
+        });
+        fileName.setText(file.getName());
+        convertView.getTag(position);
+
+        return convertView;
+    }
+}
