a63506d49bfc320bf88506ec42ba1c5502eb9a42
code2Vici <naborp@cpp.edu>
2017-04-21 16:29:17 -0700

    Updated Map Activity

--- CPPTourApp/app/src/main/java/siliconsolutions/cpptourapp/BaseMap.java
+++ CPPTourApp/app/src/main/java/siliconsolutions/cpptourapp/BaseMap.java
@@ -1,20 +1,46 @@
 package siliconsolutions.cpptourapp;
 
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
-/**
- * Created by naborp on 4/20/2017.
- */
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class BaseMap extends Activity {
+public class BaseMap extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.base_map_activity);
+        setContentView(R.layout.activity_base_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng cpp = new LatLng(34, -118);
+        mMap.addMarker(new MarkerOptions().position(cpp).title("Marker in Cal Poly Pomona"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cpp));
     }
 }
