e60b15685524def60d7fe3dd650a8796f637ff51
cuibaps1 <jadanngo@gmail.com>
2017-04-28 01:44:31 -0700

    GPS

--- CPPTourApp/app/src/main/java/siliconsolutions/cpptourapp/Activity/BaseMap.java
+++ CPPTourApp/app/src/main/java/siliconsolutions/cpptourapp/Activity/BaseMap.java
@@ -1,9 +1,17 @@
-package siliconsolutions.cpptourapp;
+package siliconsolutions.cpptourapp.Activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,6 +23,9 @@ import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,10 +35,29 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class BaseMap extends AppCompatActivity implements OnMapReadyCallback {
+
+import java.io.IOException;
+import java.util.List;
+
+import siliconsolutions.cpptourapp.GPS.GPSTracker;
+import siliconsolutions.cpptourapp.GPS.GPSTrackerListener;
+import siliconsolutions.cpptourapp.Model.GlobalVars;
+import siliconsolutions.cpptourapp.Model.MyLocation;
+import siliconsolutions.cpptourapp.R;
+
+public class BaseMap extends AppCompatActivity implements
+        OnMapReadyCallback,
+        View.OnClickListener,
+        GPSTrackerListener {
 
     private GoogleMap mMap;
     private DrawerLayout drawer;
+
+    private ImageView btnMyLocation;
+    private ImageView btnOpenFavoriteDrawer;
+
+    private GPSTracker gpsTracker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,7 +119,39 @@ public class BaseMap extends AppCompatActivity implements OnMapReadyCallback {
 
         });
         NavigationView rightNavigationView = (NavigationView) findViewById(R.id.nav_view_right);
+
+        initialize();
+
     }
+
+
+    private void initialize() {
+        initComponents();
+        initGPS();
+    }
+
+    private void initGPS() {
+        gpsTracker = new GPSTracker(this, this);
+        if (gpsTracker.canGetLocation()) {
+            double lat = gpsTracker.getLatitude();
+            double lng = gpsTracker.getLongitude();
+            Toast.makeText(getApplicationContext(), lat + " " + lng, Toast.LENGTH_SHORT).show();
+            if (lat != 0 || lng != 0) {
+                GlobalVars.location = new MyLocation(lat, lng);
+            }
+        } else {
+            gpsTracker.showSettingsAlert();
+        }
+    }
+
+    private void initComponents() {
+        btnMyLocation = (ImageView) findViewById(R.id.btnMyLocation);
+        btnMyLocation.setOnClickListener(this);
+
+        btnOpenFavoriteDrawer = (ImageView) findViewById(R.id.btnOpenFavoriteDrawer);
+        btnOpenFavoriteDrawer.setOnClickListener(this);
+    }
+
 
     /**
      * Manipulates the map once available.
@@ -105,17 +167,14 @@ public class BaseMap extends AppCompatActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng cpp = new LatLng(34, -118);
-        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(34.057551, -117.820204));
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
-        setMarkers();
-        mMap.moveCamera(center);
-        mMap.animateCamera(zoom);
+        LatLng cpp = new LatLng(34.056502, -117.821465);
+        mMap.addMarker(new MarkerOptions().position(cpp).title("Cal Poly Pomona"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cpp, 16));
     }
 
-    public void setMarkers(){
+    public void setMarkers() {
         LatLng building8 = new LatLng(34.058664, -117.824796);
-        mMap.addMarker(new MarkerOptions().position(building8).title("Building 8"));
+        //mMap.addMarker(new MarkerOptions().position(building8).title("Building 8"));
     }
 
     @Override
@@ -129,7 +188,7 @@ public class BaseMap extends AppCompatActivity implements OnMapReadyCallback {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if(id == R.id.action_openRight){
+        if (id == R.id.action_openRight) {
             drawer.openDrawer(GravityCompat.END);
             return true;
         }
@@ -151,13 +210,54 @@ public class BaseMap extends AppCompatActivity implements OnMapReadyCallback {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }
-        else if(drawer.isDrawerOpen(GravityCompat.END)){
+        } else if (drawer.isDrawerOpen(GravityCompat.END)) {
             drawer.closeDrawer(GravityCompat.END);
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        gpsTracker = new GPSTracker(this, this);
+        switch (v.getId()) {
+            case R.id.btnOpenFavoriteDrawer:{
+
+                break;
+            }
+
+            case R.id.btnMyLocation: {
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                mMap.setMyLocationEnabled(true);
+                if (gpsTracker.canGetLocation()) {
+                    double lat = gpsTracker.getLatitude();
+                    double lng = gpsTracker.getLongitude();
+                    LatLng userLatLng = new LatLng(lat, lng);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 16));
+                }
+
+            }
+        }
+    }
+
+
+    @Override
+    public void onGPSTrackerLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onGPSTrackerStatusChanged(String provider, int status, Bundle extra) {
+
+    }
 }
